6a9992256c2a7d67f96ed286242da3f0f6f39108
Swat12I <159709089+Swat12I@users.noreply.github.com>
2024-06-10 21:35:21 +0530

    Add files via upload

---
+++ git.js
@@ -0,0 +1,226 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    {
+    numb: 6,
+    question: "How many days makes a week ?",
+    answer: "7 days",
+      options: [
+       "10 days",
+        "14 days",
+        "5 days",
+        "7 days",
+   ]
+   },
+    {
+        numb: 7,
+        question: "How many players are allowed on a soccer pitch ?",
+        answer: "11 players",
+        options: [
+            "10 players",
+            "11 players",
+            "9 players",
+            "12 players",
+        ]
+    },
+    {
+        numb: 8,
+        question: "Who was the first President of USA ?",
+        answer: "George Washington",
+        options: [
+             "Donald Trump",
+            "Barack Obama",
+            "Abraham Lincoln",
+            "George Washington",
+        ]
+    },
+    {
+        numb: 9,
+        question: "30 days has ______ ?",
+        answer: "June",
+        options: [
+            "January",
+            "December",
+            "June",
+            "August",
+        ]
+    },
+    {
+        numb: 10,
+        question: "How manay hours can be found in a day ?",
+        answer: "24 hours",
+        options: [
+            "30 hours",
+            "38 hours",
+            "48 hours",
+            "24 hours",
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which is the longest river in the world ?",
+        answer: "Long River",
+        options: [
+            "River Nile",
+            "Long River",
+            "River Niger",
+            "Lake Chad",
+        ]
+    },
+    {
+        numb: 12,
+        question: "_____ is the hottest Continent on Earth ?",
+        answer: "North America",
+        options: [
+            "Oceania",
+            "Antarctica",
+            "Africa",
+            "North America",
+        ]
+    },
+    {
+        numb: 13,
+        question: "Which country is the largest in the world ?",
+        answer: "Russia",
+        options: [
+            "Russia",
+            "Canada",
+            "Africa",
+            "Egypt",
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which of these numbers is an odd number ?",
+        answer: "Eleven",
+        options: [
+            "Ten",
+            "Twelve",
+            "Eight",
+            "Eleven",
+        ]
+    },
+    {
+        numb: 15,
+        question: `"You Can't see me" is a popular saying by`,
+        answer: "John Cena",
+        options: [
+             "Eminem",
+            "Bill Gates",
+            "Chris Brown",
+            "John Cena",
+        ]
+    },
+    {
+        numb: 16,
+        question: "Where is the world tallest building located ?",
+        answer: "Dubai",
+        options: [
+            "Africa",
+            "California",
+            "Dubai",
+            "Italy",
+        ]
+    },
+    {
+        numb: 17,
+        question: "The longest river in the United Kingdom is ?",
+        answer: "River Severn",
+        options: [
+            "River Severn",
+            "River Mersey",
+            "River Trent",
+            "River Tweed",
+        ]
+    },
+    {
+        numb: 18,
+        question: "How many permanent teeth does a dog have ?",
+        answer: "42",
+        options: [
+            "38",
+            "42",
+            "40",
+            "36",
+        ]
+    },
+    {
+        numb: 19,
+        question: "Which national team won the football World cup in 2018 ?",
+        answer: "France",
+        options: [
+            "England",
+            "Brazil",
+            "Germany",
+            "France",
+        ]
+    },
+    {
+        numb: 20,
+        question: "Which US state was Donald Trump Born ?",
+        answer: "New Jersey",
+        options: [
+            "New York",
+             "California",
+            "New Jersey",
+            "Los Angeles",
+        ]
+    },
+]
